5c2ec0f08f00c397f68783ba5d53402d84a77ce4
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-04-07 22:59:28 +0330

    q3,q4

---
+++ quiz3.js
@@ -0,0 +1,21 @@
+ //? 3. Write a code to implement search on array items and returns the answers in an array
+// hint: the search is only based on the first letters of the word
+// Example:
+const brands = [
+  'dell',
+  'hp',
+  'asus',
+  'lenovo',
+  'apple',
+  'acer',
+  'samsung',
+  'sony',
+];
+function brandsSearch(arr, query) {
+
+  return arr.filter((num)=> num.toLowerCase().includes(query.toLowerCase())); //  to be completed
+}
+
+console.log(brandsSearch(brands, 'a')); // result : [ 'asus', 'apple', 'acer' ]
+console.log(brandsSearch(brands, 'sa')); // result : [ 'samsung' ]
+console.log(brandsSearch(brands, 'son')); // result : [ 'sony' ]
---
+++ quiz4.js
@@ -0,0 +1,6 @@
+//? 4. Write an array with five elements for this function in order to the result become true
+
+const array = [12,15,18,11,20]; // to be completed
+
+// DO NOT modify below code
+console.log(array.every((num) => num > 10));
